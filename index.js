09db817b99bcfdaa91a400e7c06cb7b96c827b7d
Anthony Batista <31488847+Yeffry921@users.noreply.github.com>
2021-01-27 22:58:07 -0500

    Update index.js
    remove deprecated comments/variables.

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 const gameShowcase = (() => {
-	const gameData = {
-		gameList: [],
-		nextURL: '',
-		previous: ''
-	};
-
 	////////// DATA FETCHING//////////
 	const getDefaultGames = async () => {
 		const key = '4b75a0922bbd41619c3a135bb234a3a5';
@@ -23,122 +17,38 @@ const gameShowcase = (() => {
 
 		const { results } = gameResult;
 
-    const autoComplete = document.querySelector('.autocom-box');
+    		const autoComplete = document.querySelector('.autocom-box');
     
-    if (!results) {
-      document.querySelector('.search-input').classList.remove('is-active');
-      return;
-    }
+    		if (!results) {
+      			document.querySelector('.search-input').classList.remove('is-active');
+      			return;
+    		}
 
-    autoComplete.innerHTML = '';
-    document.querySelector('.search-input').classList.add('is-active');
+    		autoComplete.innerHTML = '';
+    		document.querySelector('.search-input').classList.add('is-active');
 
-    for(let item of results) {
-      const option = document.createElement('a');
-      const gameImg = document.createElement('img');
+    		for(let item of results) {
+     			const option = document.createElement('a');
+      		const gameImg = document.createElement('img');
 
-      gameImg.src = item.background_image;
+      		gameImg.src = item.background_image;
 
-      option.textContent = item.name;
-      option.prepend(gameImg)
+      		option.textContent = item.name;
+      		option.prepend(gameImg)
 
-			autoComplete.appendChild(option);
-    }
+					autoComplete.appendChild(option);
+    	}
 	};
 
 	const getPage = async (url) => {
 		const response = await fetch(url);
 		return response.json();
 	};
-
-	////////// HELPER FUNCTIONS //////////
-	// const renderMenuGames = (games) => {
-	// 	const gameSection = document.querySelector('.game-section');
-	// 	gameSection.innerHTML = '';
-
-	// 	games.map((game) => {
-	// 		const anchor = document.createElement('a');
-	// 		const imgDiv = document.createElement('div');
-	// 		const titleDiv = document.createElement('div');
-
-	// 		imgDiv.className = 'img';
-	// 		titleDiv.className = 'title';
-
-	// 		imgDiv.style.backgroundImage = `url(${game.background_image})`;
-	// 		titleDiv.textContent = game.name;
-
-	// 		anchor.appendChild(imgDiv);
-	// 		anchor.appendChild(titleDiv);
-	// 		gameSection.appendChild(anchor);
-	// 	});
-	// };
-
-	// const fetchAndRenderGames = () => {
-	// 	getDefaultGames().then((res) => {
-	// 		gameData.gameList = res.results;
-	// 		gameData.nextURL = res.next;
-	// 		gameData.previous = res.previous;
-
-	// 		renderMenuGames(gameData.gameList);
-	// 	});
-	// };
-
-	// const onLoad = () => {
-	// 	fetchAndRenderGames();
-	// };
-
-	// const filterConsole = (gamesList, console) => {
-	// 	return gamesList.filter((game) => {
-	// 		// Get that individual games platforms
-	// 		const gamePlatforms = game.parent_platforms;
-	// 		// Check through each platform to to see if PC is included?
-	// 		const platformCheck = gamePlatforms.some((platform) => {
-	// 			return platform.platform.name === console;
-	// 		});
-
-	// 		if (platformCheck) {
-	// 			return game;
-	// 		}
-	// 	});
-	// };
-
 	////////// EVENT LISTENERS //////////
 	document.querySelector('.search').addEventListener('input', (e) => {
 		searchGame(e.target.value);
 	});
 
-	// document.querySelector('.platform-section').addEventListener('click', (e) => {
-	// 	if (e.target.classList.contains('platforms')) {
-	// 		const consoleName = e.target.dataset.console;
-
-	// 		const result = filterConsole(gameData.gameList, consoleName);
-	// 		renderMenuGames(result);
-	// 	}
-	// });
-
-	// document.querySelector('.next-showcase').addEventListener('click', (e) => {
-	// 	if (e.target.classList.contains('next')) {
-	// 		getPage(gameData.nextURL).then((res) => {
-	// 			gameData.gameList = res.results;
-	// 			gameData.nextURL = res.next;
-	// 			gameData.previous = res.previous;
-
-	// 			renderMenuGames(res.results);
-	// 		});
-	// 	} else if (e.target.classList.contains('previous')) {
-	// 		getPage(gameData.previous).then((res) => {
-	// 			gameData.gameList = res.results;
-	// 			gameData.nextURL = res.next;
-	// 			gameData.previous = res.previous;
-
-	// 			renderMenuGames(res.results);
-	// 		});
-	// 	}
-	// });
-
-	// return {
-	// 	onLoad
-	// };
 })();
 
 // gameShowcase.onLoad();
